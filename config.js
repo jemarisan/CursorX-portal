3a954b79c467c20fbbe44fd0a805884ef89c1f1b
yuanmg <1042421622@qq.com>
2025-10-11 20:33:08 +0800

    Release v1.2.2 - 公证的 DMG、配置和 appcast.xml 更新

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
     // 基本信息
     app: {
         name: 'CursorX',
-        version: 'v1.2.1',
+        version: 'v1.2.2',
         description: '让你工作更舒服的温柔贴心光标小工具',
         tagline: '像不存在一样完美的光标伴侣',
         logo: {
@@ -37,9 +37,9 @@ const CONFIG = {
         fileSize: '2.4M',
         systemRequirements: 'macOS 12.0 或更高版本 • 支持 Apple Silicon 和 Intel 芯片',
         downloadUrls: {
-            universal: 'https://raw.githubusercontent.com/jemarisan/CursorX-portal/main/downloads/CursorX-v1.2.1.dmg',
-            'apple-silicon': 'https://raw.githubusercontent.com/jemarisan/CursorX-portal/main/downloads/CursorX-v1.2.1.dmg',
-            intel: 'https://raw.githubusercontent.com/jemarisan/CursorX-portal/main/downloads/CursorX-v1.2.1.dmg'
+            universal: 'https://raw.githubusercontent.com/jemarisan/CursorX-portal/main/downloads/CursorX-v1.2.2.dmg',
+            'apple-silicon': 'https://raw.githubusercontent.com/jemarisan/CursorX-portal/main/downloads/CursorX-v1.2.2.dmg',
+            intel: 'https://raw.githubusercontent.com/jemarisan/CursorX-portal/main/downloads/CursorX-v1.2.2.dmg'
         }
     },
     
